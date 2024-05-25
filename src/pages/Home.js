e7b24e787d38954518b471caaf13355eaad55185
Harshit Sachan <sachanharshit80@gmail.com>
2024-05-25 21:45:22 +0530

    Initial

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ParticleBackground from '../components/ParticleBackground';
+function Home() {
+  return (
+    <div className="home-container">
+    <ParticleBackground />
+    <div className="content">
+    
+    </div>
+  </div>
+ 
+  )
+
+  
+}
+
+export default Home;
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const links = [
+    { path: "/", text: "Home" },
+    { path: "/about", text: "About" },
+    { path: "/skills", text: "Skills" },
+    { path: "/projects", text: "Projects" }
+  ];
