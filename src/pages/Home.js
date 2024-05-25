29c9172d4b7fd9d881f38915d04270bb4bb093ef
Harshit Sachan <sachanharshit80@gmail.com>
2024-05-26 01:41:44 +0530

    Second

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ParticleBackground from '../components/ParticleBackground';
+import '../styles/Home.css';
 function Home() {
   return (
     <div className="home-container">
     <ParticleBackground />
     <div className="content">
-    
+      <h1>Harshit Sachan</h1>
+      <p>Web Developer</p>
     </div>
   </div>
- 
   )
-
-  
 }
 
 export default Home;
