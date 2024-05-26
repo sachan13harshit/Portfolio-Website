9aaa07bd06357f043ec8da078e934a296a3e26a9
Harshit Sachan <sachanharshit80@gmail.com>
2024-05-26 13:27:18 +0530

    Third

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function About() {
-  return <div>About Page</div>;
+  return (
+    <h1>  </h1>
+  )
 }
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ParticleBackground from '../components/ParticleBackground';
 import '../styles/Home.css';
+import profile from '../utils/profile.png';
+import Type from '../components/Type';
 function Home() {
+  const imageStyle = {
+    objectFit: "shrink",
+    width: ["13rem", "13rem", "8rem"],
+    height: ["13rem", "13rem", "15rem"],
+    marginTop: ["5rem", "5rem", null],
+    marginBottom: ["1rem", "1rem", null],
+    backgroundColor: "blue", // Replace with your theme color variable
+    borderRadius: "30rem",
+  };
   return (
     <div className="home-container">
     <ParticleBackground />
     <div className="content">
-      <h1>Harshit Sachan</h1>
-      <p>Web Developer</p>
+        <div className="row">
+          <div className="home-image">
+           <img className="profile-image" src={profile} alt="profile" />
+          </div>
+          <div className="home-down">
+            <div className="home-text">
+              <h1>
+                Hi , my name is
+              </h1>
+            </div>
+            <div>
+            <h1 className="heading-name">
+              I'M <strong className="main-name">Harshit Sachan</strong>
+            </h1>
+            </div>
+            <div className="type-container">
+            <h1>
+              I am a <strong className="type"> <Type /></strong>
+            </h1>
+           </div>
+          </div>
+        </div>
     </div>
   </div>
   )
